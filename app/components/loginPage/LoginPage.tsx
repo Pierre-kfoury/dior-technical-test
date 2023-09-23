964984f898076129ce79ef96c1a9a55152f5d8d5
Pierre Kfoury <pierrekfoury@mbp-de-pierre.home>
2023-09-23 11:01:16 +0200

    Add new hooks and tablet view

--- app/components/loginPage/LoginPage.tsx
+++ app/components/loginPage/LoginPage.tsx
@@ -1,34 +1,43 @@
+"use client";
 import { DiorIcon } from "@/app/icons/DiorIcon";
 import { LoginButton } from "../loginButton/LoginButton";
 import { Welcome } from "../welcome/Welcome";
 import { Input } from "../input/Input";
 import Image from "next/image";
+import { useMediaQuerrySSR } from "@/app/hooks/useMediaQuerrySSR";
 
 export function LoginPage() {
-  return (
-    <div>
-      <div className="section-0">
+  const { mediaQuery, mounted } = useMediaQuerrySSR();
+
+  return mounted ? (
+    <>
+      <div className={`section-0-${mediaQuery}`}>
         <DiorIcon />
       </div>
       <div className="main">
-        <div className="section-1">
+        <div className={`section-1-${mediaQuery}`}>
           <Welcome />
-          <div className="section-2-1">
+          <div className={`section-2-1-${mediaQuery}`}>
             <Input />
           </div>
-          <div className="section-2-2">
+          <div className={`section-2-2-${mediaQuery}`}>
             <LoginButton title="LOGIN" />
           </div>
         </div>
-        <div className="section-3">
+        <div className={`section-3-${mediaQuery}`}>
           <Image
-            src="/eiffel-tower-L.png"
+            priority
+            src={
+              mediaQuery === "mobile"
+                ? "/eiffel-tower-S.png"
+                : "/eiffel-tower-L.png"
+            }
             width={515}
             height={953}
             alt="eiffel-tower"
           />
         </div>
       </div>
-    </div>
-  );
+    </>
+  ) : null;
 }
---
+++ app/hooks/useMediaQuerrySSR.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { useMediaQuery } from "./useMediaQuerry";
+
+type MediaQueryType = "mobile" | "tablet" | "desktop";
+
+export function useMediaQuerrySSR() {
+  const [mounted, setMounted] = useState(false);
+
+  const isMobile = useMediaQuery("(min-width: 390px) and (max-width: 926px)"); // Iphone 14
+  const isTablet = useMediaQuery("(min-width: 834px) and (max-width: 1194px)"); // Ipad Pro 11
+  const isDesktop = useMediaQuery("(min-width: 1200px)"); // Desktop
+
+  const mediaQuery = getMediaQuery(isMobile, isTablet, isDesktop);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return { mediaQuery, mounted };
+}
+
+function getMediaQuery(
+  isMobile: boolean,
+  isTablet: boolean,
+  isDesktop: boolean
+): MediaQueryType {
+  if (isMobile) return "mobile";
+  if (isTablet) return "tablet";
+  if (isDesktop) return "desktop";
+  return "desktop";
+}
